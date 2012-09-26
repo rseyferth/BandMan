4584d085a9b86ab79a63bdd572e3c259e37a0df7
Ruben Seyferth <ruben.seyferth@gmail.com>
2012-09-26 14:40:24 +0200

    Add avatar to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120918130315) do
+ActiveRecord::Schema.define(:version => 20120926123732) do
 
   create_table "band_genres", :force => true do |t|
     t.integer  "band_id"
